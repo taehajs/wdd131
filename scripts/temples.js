42128cb7329dbafc35fa671941583da0621dcdb8
Kim JiSeung <taehajs@naver.com>
2025-09-12 20:50:10 +0900

    dd

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,15 +1,15 @@
-document,getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
 
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', () => {
-    if (navMenu.style.display === 'flex') {
-        navMenu.style.display = 'none';
-        hamburger.textContent = '☰';
-    } else {
-        navMenu.style.display = 'flex';
-        hamburger.textContent = '✖';
-    }
+  if (navMenu.style.display === 'flex') {
+    navMenu.style.display = 'none';
+    hamburger.textContent = '☰'; 
+  } else {
+    navMenu.style.display = 'flex';
+    hamburger.textContent = '✖'; 
+  }
 });
